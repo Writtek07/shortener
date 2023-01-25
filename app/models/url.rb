b0b3ef659d76d72204db80eaa6f3d486db595a1c
Writtek <writtek@weinvests-MacBook-Pro.local>
2023-01-25 22:27:37 +0530

    Logic in controller and View updates

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,3 +1,11 @@
 class Url < ApplicationRecord
-	
+	validates :original_url, presence:true
+
+	before_create :generate_short_url
+
+	  private
+
+	  def generate_short_url
+	    self.short_url = SecureRandom.urlsafe_base64(6)
+	  end
 end
